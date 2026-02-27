2b74491069a8161eb5f65c3390b62f9a3b205886
Osgar Developer <osgar@webinno.io>
2026-02-27 23:57:27 +0100

    Set spacing to 1px below Header and DonationCard

--- app/page.tsx
+++ app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
       <div className="w-full max-w-[980px] flex flex-col gap-[18px]">
         <Header />
 
-        <div style={{ marginBottom: '5px' }}></div>
+        <div style={{ marginBottom: '1px' }}></div>
 
         {error && (
           <div
@@ -100,7 +100,7 @@ export default function Home() {
         </div>
 
         <DonationCard />
-        <div style={{ marginBottom: '5px' }}></div>
+        <div style={{ marginBottom: '1px' }}></div>
       </div>
     </main>
   )
